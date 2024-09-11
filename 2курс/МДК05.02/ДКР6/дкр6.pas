51e744018ca4a0309bc1b3e01562536a6daaf52e
Egor <143870164+nikulb855@users.noreply.github.com>
2024-09-11 16:00:08 +0300

    Add files via upload

---
+++ 2курс/МДК05.02/ДКР6/дкр6.pas
@@ -0,0 +1,112 @@
+﻿program Кольцевойдвусвязныйсписок;
+
+const
+  MAX_SIZE = 10;
+
+type
+  Node = record
+    data: integer;
+    next, prev: integer;
+  end;
+
+var
+  list: array [1..MAX_SIZE] of Node;
+  head, tail, free: integer;
+
+procedure InitializeList();
+var
+  i: integer;
+begin
+
+  for i := 1 to MAX_SIZE do
+  begin
+    list[i].next := i + 1;
+    list[i].prev := i - 1;
+  end;
+  list[MAX_SIZE].next := 1; 
+  list[1].prev := MAX_SIZE;
+  head := 0; 
+  free := 1; 
+end;
+
+function CreateNode(value: integer): integer;//добавить элемент в список
+var
+  newNode: integer;
+begin
+  if free = 0 then 
+  begin
+    writeln('Не хватает памяти');
+    Halt;
+  end;
+  newNode := free; 
+  free := list[free].next; 
+  list[newNode].data := value; 
+  CreateNode := newNode; 
+end;
+
+procedure InsertFront(value: integer); 
+var
+  newNode: integer;
+begin
+  newNode := CreateNode(value); 
+  if head = 0 then 
+  begin
+    head := newNode;
+    tail := newNode;
+  end
+  else
+  begin
+    list[newNode].next := head; 
+    list[head].prev := newNode; 
+    head := newNode; 
+  end;
+end;
+
+procedure DisplayList(); //показать список
+var
+  currentNode: integer;
+begin
+  if head = 0 then 
+  begin
+    writeln('Список пуст');
+    Exit;
+  end;
+  currentNode := head;
+  repeat
+    if (list[currentNode].data <> 0) then 
+      writeln('Data: ', list[currentNode].data);
+    currentNode := list[currentNode].next;
+  until currentNode = head; 
+end;
+
+procedure Menu(); //case menu
+var
+  choice, value: integer;
+begin
+  repeat
+    writeln('1. Вставить элемент в начало списка');
+    writeln('2. Вывести список');
+    writeln('3. Выйти из программы');
+    writeln('Выберите действие: ');
+    readln(choice);
+    case choice of
+      1: begin
+           writeln('Введите значение для нового элемента: ');
+           readln(value);
+           InsertFront(value);
+         end;
+      2: begin
+           writeln('Список:');
+           DisplayList();
+         end;
+      3: writeln('Выход из программы');
+    else
+      writeln('Неверный ввод. Пожалуйста, выберите существующий пункт меню.');
+    end;
+  until choice = 3;
+end;
+
+begin
+  InitializeList();
+  Menu();
+end.
---
+++ 2курс/МДК05.02/ДКР7/дкр7.pas
@@ -0,0 +1,60 @@
+﻿uses GraphABC;
+var x1,y1,ll,tt:integer;
+ 
+procedure Draw(x, y, l, u : Real; t : Integer); 
+procedure Draw2(Var x, y: Real; l, u : Real; t : Integer);
+begin
+  Draw(x, y, l, u, t);
+  x := x + l*cos(u);
+  y := y - l*sin(u);
+end;
+ 
+begin
+  if t > 0 then begin
+    l := l/3;
+    Draw2(x, y, l, u, t-1);
+    Draw2(x, y, l, u+pi/3, t-1);
+    Draw2(x, y, l, u-pi/3, t-1);
+    Draw2(x, y, l, u, t-1);
+  end
+  else Line(Round(x), Round(y), Round(x+cos(u)*l), Round(y-sin(u)*l));
+end;
+ 
+Procedure snezhinka; 
+var x2,y2,x3,y3: real;
+begin 
+  Window.Clear; 
+  x3 := x1+cos(pi/3)*ll;
+  y3 := y1-sin(pi/3)*ll;
+  x2 := x3+cos(-pi/3)*ll;
+  y2 := y3-sin(-pi/3)*ll;
+  Draw(X1, Y1, ll, pi/3, tt);
+  Draw(x2, y2, ll, pi, tt);
+  Draw(x3, y3, ll, -pi/3, tt);    
+  Redraw;
+end;
+ 
+Procedure KeyDown(k: integer);
+begin
+ case K of
+    VK_Down: y1 := y1+10; //курсоры в четыре стороны 
+    VK_Up: y1 := y1-10; 
+    VK_Left: x1 := x1-10;  
+    VK_Right: x1 := x1+10;
+    VK_Escape: halt(1); //выход
+    VK_A: if ll < 1000 then ll := ll+10; //клавиша A = масштаб +
+    VK_Z: if ll > 50 then ll := ll-10; //клавиша Z = масштаб -
+    VK_S: if tt < 7 then tt := tt+1; //клавиша S = ломаных + 
+    VK_X: if tt > 0 then tt := tt-1; //клавиша X = ломаных -
+   end; 
+   snezhinka;
+end;
+ 
+begin 
+  SetWindowSize(800,800);
+  SetWindowCaption('Фрактал: Снежинка Коха');   
+  LockDrawing;
+  x1:=200; y1:=500; ll:=400; tt:=2;
+  snezhinka; 
+  OnkeyDown += KeyDown;
+end.
